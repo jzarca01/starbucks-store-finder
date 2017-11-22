9261405d8fd1087f10e79be83cf8f7c4ae2dc797
Jérémie Zarca <jzarca01@users.noreply.github.com>
2017-11-22 19:48:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
 'use strict'
+const fetch = require('node-fetch');
 
-const cheerio = require('cheerio')
-const request = require('request-promise-native')
+module.exports = ({ lat, lon } = {} => {
+    const apiUrl = `https://www.starbucks.fr/bff/locations?lat=${lat}&lng=${lon}`;
 
-module.exports = ({ lat, lon } = {}) => {
-    return new Promise((resolve, reject) => {
-      
-        if (!lat || !lon) {
-        return reject(new TypeError('Options are required'))
+    const options = { 
+        method: 'GET',
+        headers: 
+        { 
+            'x-requested-with': 'XMLHttpRequest',
+            accept: 'application/json' 
         }
-        
-        const options = { 
-          method: 'GET',
-          url: 'https://www.starbucks.fr/bff/locations',
-          qs: { lat: lat, lng: lon },
-          headers: 
-          { 
-              'x-requested-with': 'XMLHttpRequest',
-              accept: 'application/json' 
-          }
-        };
+    };
 
-        request(options).then(res => {
-        if (res) {
-            resolve(JSON.parse(stores));
-        }
-        reject(
-            new TypeError(`Couldn't find any Starbucks store on this location`)
-        )
-        })
-        .catch(err => console.log);
-    });
-}
+    return fetch(apiUrl, options)
+    .then(res => res.text())
+    .then(res => JSON.parse(res))
+    .catch(err => console.log(err))
+});
