276a4444f36430b1e2dfe5511d79eb46579326a2
Jérémie Zarca <jzarca01@users.noreply.github.com>
2019-07-24 17:25:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
-module.exports = (coordinates => {
-    return getStores(coordinates);
-});
-
-function getStores({lat, lng}) {
-    const apiUrl = `https://www.starbucks.fr/bff/locations?lat=${lat}&lng=${lng}`;
-    
-    const options = { 
-        method: 'GET',
-        headers: 
-        { 
-            'x-requested-with': 'XMLHttpRequest',
-            accept: 'application/json' 
-        }
-    };
-    
-    return fetch(apiUrl, options)
-    .then(res => res.text())
-    .then(res => JSON.parse(res))
-    .then(res => res.stores)
-    .catch(err => [])
+const getStores = async ({lat, lng}) => {
+    try {
+        const sb = await axios({
+            method: 'GET',
+            url: `https://www.starbucks.fr/bff/locations?lat=${lat}&lng=${lng}`,
+            headers: {
+                    'x-requested-with': 'XMLHttpRequest',
+                    accept: 'application/json' 
+                }
+            });
+        
+        const { stores } = sb.data;
+        return stores;
+    }
+    catch(err) {
+        return [];
+    })
 }
+
+module.exports = getStores;
